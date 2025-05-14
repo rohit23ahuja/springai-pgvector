ba417da57d3da310d01751dbe26cf3a62f7c67b3
Rohit Ahuja <rohit23ahuja@gmail.com>
2025-05-14 17:00:51 +0530

    initial commit

---
+++ src/main/java/dev/rohitahuja/SpringaiPgvectorApplication.java
@@ -0,0 +1,13 @@
+package dev.rohitahuja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiPgvectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiPgvectorApplication.class, args);
+	}
+
+}
